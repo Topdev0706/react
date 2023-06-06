f390c1f17765cfa92d04997a8039596dc4f1dcf6
Topdev0706 <jacksontomas731@gmail.com>
2023-06-06 12:04:50 +0900

    Add: new class: TempMain
    A temporary main class in the development of this application.

---
+++ src/temp/TempMain.java
@@ -0,0 +1,10 @@
+package temp;
+
+/**
+ * A temporary main class in the development of this application.
+ */
+public class TempMain {
+	public static void main(String[] args) {
+		System.out.println("Hello RINEARN Graph 3D Ver.6!");
+	}
+}
