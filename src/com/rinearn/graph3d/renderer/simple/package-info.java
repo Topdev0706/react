31cccee805acea5a9cc1f2d6c79a42d983ad5e79
Topdev0706 <jacksontomas731@gmail.com>
2023-06-07 14:48:43 +0900

    Add: new package: com.rinearn.graph3d.renderer.simple
    Providing a simple implementation of
    the rendering engine of RINEARN Graph 3D.

---
+++ src/com/rinearn/graph3d/renderer/simple/package-info.java
@@ -0,0 +1,11 @@
+package com.rinearn.graph3d.renderer.simple;
+
+/**
+ * <span class="lang-en">
+ * The package providing a simple implementation of the rendering engine of RINEARN Graph 3D
+ * </span>
+ * <span class="lang-ja">
+ * RINEARN Graph 3D の描画エンジンの、シンプルな実装を提供するパッケージです
+ * </span>
+ * .
+ */
