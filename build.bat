1c7537b24bb13180a7c7e0d5f894685f4849ad77
Topdev0706 <jacksontomas731@gmail.com>
2023-07-05 14:20:29 +0900

    Fix: build scripts

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 mkdir bin
 cd src
-javac @com/rinearn/graph3d/sourcelist.txt @temp/sourcelist.txt -d ../bin -encoding UTF-8
+javac @com/rinearn/graph3d/sourcelist.txt -d ../bin -encoding UTF-8
+javac @temp/sourcelist.txt -d ../bin -encoding UTF-8
 cd ..
