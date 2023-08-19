f4dc380abe0bb4bd57360a4ef7a549a81b7a03f0
Topdev0706 <jacksontomas731@gmail.com>
2023-08-19 20:11:20 +0900

    Add: RinearnGraph3D: create a renderer and display a 3D graph

--- src/com/rinearn/graph3d/RinearnGraph3D.java
+++ src/com/rinearn/graph3d/RinearnGraph3D.java
@@ -1,6 +1,11 @@
 package com.rinearn.graph3d;
 
 import com.rinearn.graph3d.view.View;
+import com.rinearn.graph3d.renderer.RinearnGraph3DRenderer;
+import com.rinearn.graph3d.renderer.simple.SimpleRenderer;
+
+import java.awt.Image;
+
 
 /**
  * <spac lass="lang-en">
@@ -19,6 +24,9 @@ public class RinearnGraph3D {
 	/** The front-end class of "View" layer, which provides visible part of GUI without event handling. */
 	private volatile View view;
 
+	/** The rendering engine of 3D graphs. */
+	private volatile RinearnGraph3DRenderer renderer;
+
 
 	/**
 	 * <span class="lang-en">
@@ -29,9 +37,45 @@ public class RinearnGraph3D {
 	 * </span>
 	 */
 	public RinearnGraph3D() {
-		System.out.println("Hello! the constructor of RinearnGraph3D has been called!");
 
+		// Create "View" layer, which provides visible part of GUI without event handling.
 		this.view = new View();
+
+		// Create a rendering engine of 3D graphs.
+		this.renderer = new SimpleRenderer(
+				view.mainWindow.DEFAULT_SCREEN_WIDTH, view.mainWindow.DEFAULT_SCREEN_HEIGHT
+		);
+
+		// Set the reference to the rendered image of the renderer,
+		// to the graph screen of the window for displaying it.
+		Image screenImage = this.renderer.getScreenImage();
+		this.view.mainWindow.setScreenImage(screenImage);
+
+		// First rendering and repainting.
+		this.renderer.drawScale();
+		this.renderer.drawGrid();
+		this.renderer.drawFrame();
+		this.renderer.render();
+		this.view.mainWindow.repaintScreen();
+
+		// Show the window.
 		view.mainWindow.setWindowVisible(true);
+	}
+
+
+	/**
+	 * <span class="lang-en">
+	 * Returns the 3D renderer, which is being used for rendering the graph image in this instance
+	 * </span>
+	 * <span class="lang-ja">
+	 * グラフ画面を描画するのに使用されている3D描画エンジン（レンダラー）を返します
+	 * </span>
+	 * .
+	 * @return
+	 *   <span class="lang-en">The 3D renderer</span>
+	 *   <span class="lang-ja">3D描画エンジン（レンダラー）</span>
+	 */
+	public synchronized RinearnGraph3DRenderer getRenderer() {
+		return this.renderer;
 	}
 }
