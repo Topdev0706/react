9c2b7161d3ac5531b5f93edaeef060cdd19a5112
Topdev0706 <jacksontomas731@gmail.com>
2023-08-19 17:42:21 +0900

    Add: new package: com.rinearn.graph3d.view
    The package composing "View" layer,
    which provides visible part of GUI
    without event handling.

---
+++ src/com/rinearn/graph3d/view/package-info.java
@@ -0,0 +1,13 @@
+package com.rinearn.graph3d.view;
+
+/**
+ * <span class="lang-en">
+ * The package composing "View" layer,
+ * which provides visible part of GUI without event handling
+ * </span>
+ * <span class="lang-ja">
+ * GUIの、目に見える部分（イベント処理などは除く）を提供する、
+ * 「View」層のパッケージです
+ * </span>
+ * .
+ */
