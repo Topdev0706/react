7190bdc0f5ef0791a02fc06e84f3edf034a04447
Topdev0706 <jacksontomas731@gmail.com>
2023-08-23 08:59:01 +0900

    Add: new package: com.rinearn.graph3d.presenter
    The package composing "Presenter" layer,
    which invokes "Model" layer's procedures triggered by user's action,
    and updates the graph screen depending on the result.

---
+++ src/com/rinearn/graph3d/presenter/package-info.java
@@ -0,0 +1,14 @@
+package com.rinearn.graph3d.presenter;
+
+/**
+ * <span class="lang-en">
+ * The package composing "Presenter" layer,
+ * which invokes "Model" layer's procedures triggered by user's action,
+ * and updates the graph screen depending on the result
+ * </span>
+ * <span class="lang-ja">
+ * ユーザーの操作に応じてModel層のロジック処理を実行し、その結果に応じてグラフ画面を更新する、
+ * 「Presenter」層のパッケージです
+ * </span>
+ * .
+ */
