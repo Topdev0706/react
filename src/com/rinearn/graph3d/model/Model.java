c5d175a6e6d85c9227ef03c4228ca39fd394d059
Topdev0706 <jacksontomas731@gmail.com>
2023-08-23 08:57:27 +0900

    Add: new class: Model
    The front-end class of "Model" layer
    (com.rinearn.graph3d.model package).

---
+++ src/com/rinearn/graph3d/model/Model.java
@@ -0,0 +1,18 @@
+package com.rinearn.graph3d.model;
+
+
+/**
+ * The front-end class of "Model" layer (com.rinearn.graph3d.model package)
+ * of RINEARN Graph 3D.
+ * 
+ * Model layer provides internal components of this software,
+ * such as various logic procedures and so on.
+ */
+public class Model {
+
+	/**
+	 * Creates new Model layer of RINEARN Graph 3D.
+	 */
+	public Model() {
+	}
+}
