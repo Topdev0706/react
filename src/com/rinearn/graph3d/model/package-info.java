fe0a8b1a47dc960ff727afbd60be89d3a99816cf
Topdev0706 <jacksontomas731@gmail.com>
2023-08-23 08:54:22 +0900

    Add: new package: com.rinearn.graph3d.model
    The package composing "Model" layer,
    which provides internal components of this software,
    such as various logic procedures and so on.

---
+++ src/com/rinearn/graph3d/model/package-info.java
@@ -0,0 +1,12 @@
+package com.rinearn.graph3d.model;
+
+/**
+ * <span class="lang-en">
+ * The package composing "Model" layer, which provides internal components of this software,
+ * such as various logic procedures and so on
+ * </span>
+ * <span class="lang-ja">
+ * 各種ロジック処理など、ソフトウェアの内部処理類を提供する「Model」層のパッケージです
+ * </span>
+ * .
+ */
