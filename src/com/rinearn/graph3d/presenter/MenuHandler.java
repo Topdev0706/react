bba311c684d6d75c6dd150051ba32f04ebbe0d99
Topdev0706 <jacksontomas731@gmail.com>
2023-09-19 20:45:32 +0900

    Add: new class: MenuHandler
    The class handling events and API requests related to the menu bar and right-click menus.

---
+++ src/com/rinearn/graph3d/presenter/MenuHandler.java
@@ -0,0 +1,45 @@
+package com.rinearn.graph3d.presenter;
+
+import com.rinearn.graph3d.model.Model;
+import com.rinearn.graph3d.view.View;
+
+/**
+ * The class handling events and API requests related to the menu bar and right-click menus.
+ */
+public final class MenuHandler {
+
+	/** The front-end class of "Model" layer, which provides internal logic procedures and so on. */
+	private final Model model;
+
+	/** The front-end class of "View" layer, which provides visible part of GUI without event handling. */
+	private final View view;
+
+	/** The front-end class of "Presenter" layer, which invokes Model's procedures triggered by user's action on GUI. */
+	private final Presenter presenter;
+
+
+	/**
+	 * Create a new instance handling events and API requests using the specified resources.
+	 * 
+	 * @param model The front-end class of "Model" layer, which provides internal logic procedures and so on.
+	 * @param view The front-end class of "View" layer, which provides visible part of GUI without event handling.
+	 * @param presenter The front-end class of "Presenter" layer, which handles events occurred on GUI, and API requests.
+	 */
+	public MenuHandler(Model model, View view, Presenter presenter) {
+		this.model = model;
+		this.view = view;
+		this.presenter = presenter;
+	}
+
+
+	/**
+	 * Sets the visibility of the menu bar and the right click menus.
+	 * 
+	 * @param visible Specify true for showing the menu bar and the right click menus.
+	 */
+	public synchronized void setMenuVisible(boolean visible) {
+		view.mainWindow.setMenuVisible(visible);
+		view.mainWindow.forceUpdateWindowLayout();
+		presenter.renderingLoop.requestRendering();		
+	}
+}
