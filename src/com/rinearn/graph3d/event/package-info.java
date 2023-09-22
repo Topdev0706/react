bebd7f15888ded36dbfe2dda5c2a3d3de1804d39
Topdev0706 <jacksontomas731@gmail.com>
2023-09-22 18:39:40 +0900

    Add: new package: com.rinearn.graph3d.event
    The package providing event-related classes and interfaces.

---
+++ src/com/rinearn/graph3d/event/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <span class="lang-ja">RINEARN Graph 3Dのイベント処理関連のクラスやインターフェースを提供するパッケージです</span>
+ * <span class="lang-en">The package providing event-related classes and interfaces of RINEARN Graph 3D</span>
+ * .
+ */
+package com.rinearn.graph3d.event;
