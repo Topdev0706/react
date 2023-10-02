767fb837f3f253d6a146858e89e125d35817e3fb
Topdev0706 <jacksontomas731@gmail.com>
2023-10-02 09:43:59 +0900

    Add: new class: ScriptEngineMount
    The class initializing and retaining script engines,
    and wrapping I/O to/from script engines.

--- src/com/rinearn/graph3d/model/Model.java
+++ src/com/rinearn/graph3d/model/Model.java
@@ -1,12 +1,10 @@
 package com.rinearn.graph3d.model;
 
 import com.rinearn.graph3d.config.RinearnGraph3DConfiguration;
-import org.vcssl.nano.VnanoEngine;
 import org.vcssl.nano.VnanoException;
-import org.vcssl.nano.interconnect.PluginLoader;
 
-import java.util.Map;
-import java.util.HashMap;
+import javax.swing.JOptionPane;
+import java.util.Locale;
 
 
 /**
@@ -21,14 +19,47 @@ public final class Model {
 	/** The container of configuration parameters. */
 	private final RinearnGraph3DConfiguration config;
 
+	/** The "engine-mount" object, initializing and retaining script engines in this application. */
+	private final ScriptEngineMount scriptEngineMount;
+
 
 	/**
 	 * Creates new Model layer of RINEARN Graph 3D.
 	 * 
 	 * @param configuration The container of configuration parameters.
+	 * @throws IllegalStateException Thrown when it failed in initializing script engines, etc.
 	 */
 	public Model(RinearnGraph3DConfiguration configuration) {
+
+		// Store the configuration container.
 		this.config = configuration;
+
+		// Extract some environment-dependent information from the configuration.
+		Locale locale = this.config.getEnvironmentConfiguration().getLocale();
+		boolean isJapanese = this.config.getEnvironmentConfiguration().isLocaleJapanese();
+
+		// Initialize script engines.
+		try {
+			this.scriptEngineMount = new ScriptEngineMount(locale);
+		} catch (VnanoException vne) {
+
+			// Create the error message to be displayed on the pop-up window.
+			String errorMessage = isJapanese ?
+					"スクリプトエンジンの起動に失敗しました:\n\n" + vne.getMessage() :
+					"Failed in starting scrpit engines:\n\n" + vne.getMessage();
+
+			// If the error message is too long, crop it.
+			int errorLength = errorMessage.length();
+			if (150 < errorLength) {
+				errorMessage = errorMessage.substring(0, 70) +
+						" ... " + errorMessage.substring(errorLength - 70, errorLength) + "\n" +
+						(isJapanese ? "(詳細はコンソールに出力)": "(Details are output to the console.)");
+			}
+
+			// Show the error message to the user, by popping-up the message window.
+			JOptionPane.showMessageDialog(null, errorMessage, "!", JOptionPane.ERROR_MESSAGE);
+			throw new IllegalStateException(vne);
+		}
 	}
 
 
@@ -54,42 +85,36 @@ public final class Model {
 		System.out.println("- Vnano Math Calculation Exam -");
 
 		try {
-
-			// Create a scripting engine of Vnano.
-			VnanoEngine engine = new VnanoEngine();
-
-			// Enable the option to handle integers in expressions as floating-point numbers.
-			Map<String, Object> optionMap = new HashMap<String, Object>();
-			optionMap.put("EVAL_INT_LITERAL_AS_FLOAT", true);
-			engine.setOptionMap(optionMap);
-
-			// Load plug-ins.
-			PluginLoader pluginLoader = new PluginLoader("UTF-8");
-			pluginLoader.setPluginListPath("./plugin/VnanoPluginList.txt");
-			pluginLoader.load();
-			for (Object plugin: pluginLoader.getPluginInstances()) {
-				engine.connectPlugin("___VNANO_AUTO_KEY", plugin);
-			}
-
-			// Calculate expressions.
 			String expression;
+			double x, y, t;
 			double result;
 
 			expression = "1 + 2 * (3 - 4 / 5)";
-			result = (double)engine.executeScript(expression + ";");
+			result = this.scriptEngineMount.calculateMathExpression(expression);
 			System.out.println(expression + " = " + result);
 
 			expression = "sin(1)";
-			result = (double)engine.executeScript(expression + ";");
+			result = this.scriptEngineMount.calculateMathExpression(expression);
 			System.out.println(expression + " = " + result);
 
 			expression = "cos(1)";
-			result = (double)engine.executeScript(expression + ";");
+			result = this.scriptEngineMount.calculateMathExpression(expression);
 			System.out.println(expression + " = " + result);
 
 			expression = "sin(1) * sin(1) + cos(1) * cos(1)";
-			result = (double)engine.executeScript(expression + ";");
+			result = this.scriptEngineMount.calculateMathExpression(expression);
 			System.out.println(expression + " = " + result);
+
+			expression = "7 * t";
+			t = 3.0;
+			result = this.scriptEngineMount.calculateMathExpression(expression, t);
+			System.out.println(expression + " = " + result + ", where t=" + t);
+
+			expression = "x*x + 2*x - 3*y + 15";
+			x = 2.0;
+			y = 3.0;
+			result = this.scriptEngineMount.calculateMathExpression(expression, x, y);
+			System.out.println(expression + " = " + result + ", where x=" + x + ", y=" + y);
 
 		} catch (VnanoException vne) {
 			vne.printStackTrace();
---
+++ src/com/rinearn/graph3d/model/ScriptEngineMount.java
@@ -0,0 +1,160 @@
+package com.rinearn.graph3d.model;
+
+import org.vcssl.connect.ConnectorPermissionName;
+import org.vcssl.connect.ConnectorPermissionValue;
+import org.vcssl.nano.VnanoEngine;
+import org.vcssl.nano.VnanoException;
+import org.vcssl.nano.interconnect.PluginLoader;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Locale;
+
+
+/**
+ * The class initializing and retaining script engines, and wrapping I/O to/from script engines.
+ */
+public final class ScriptEngineMount {
+
+	/** The script engine for calculating values of math expressions. */
+	private volatile VnanoEngine mathExpressionEngine = null;
+
+	/** The plug-in instance providing parameter variables (x, y, and t) available in math expressions. */
+	private MathExpressionParameterPlugin mathExpressionParameterPlugin = null;
+
+	/** The plug-in class providing parameter variables (x, y, and t) available in math expressions. */
+	public final class MathExpressionParameterPlugin {
+
+		// Caution: This class must be "public", to be accessed from the script engine.
+
+		/** The parameter "x" of "f(x,y)" form math expressions. */
+		public volatile double x = Double.NaN;
+
+		/** The parameter "y" of "f(x,y)" form math expressions. */
+		public volatile double y = Double.NaN;
+
+		/** The parameter "t" of "f(t)" form math expressions. */
+		public volatile double t = Double.NaN;
+	}
+
+
+	/**
+	 * Create a new instance retaining a set of script engines, by the default settings.
+	 * 
+	 * @param locale The locale of the user's environment, which determines the language of error messages.
+	 * @throws VnanoException Thrown when it failed to load/initialize plug-ins, etc.
+	 */
+	public ScriptEngineMount(Locale locale) throws VnanoException {
+		this.initializeMathExpressionEngine(locale);
+	}
+
+
+	/**
+	 * Calculate the specified math expression having no parameters.
+	 * 
+	 * @param expression The math expression to be calculated.
+	 * @return The calculated value.
+	 * @throws VnanoException Throws if any syntax error is detected for the specified expression.
+	 */
+	public synchronized double calculateMathExpression(String expression) throws VnanoException {
+		mathExpressionParameterPlugin.x = Double.NaN;
+		mathExpressionParameterPlugin.y = Double.NaN;
+		mathExpressionParameterPlugin.t = Double.NaN;
+		double calculatedValue = (double)this.mathExpressionEngine.executeScript(expression + ";");
+		return calculatedValue;
+	}
+
+
+
+	/**
+	 * Calculate the specified math expression of "f(x,y)" form.
+	 * 
+	 * @param expression The math expression to be calculated.
+	 * @param x The parameter "x" of "f(x,y)".
+	 * @param y The parameter "y" of "f(x,y)".
+	 * @return The calculated value.
+	 * @throws VnanoException Throws if any syntax error is detected for the specified expression.
+	 */
+	public synchronized double calculateMathExpression(String expression, double x, double y) throws VnanoException {
+		mathExpressionParameterPlugin.x = x;
+		mathExpressionParameterPlugin.y = y;
+		mathExpressionParameterPlugin.t = Double.NaN;
+		double calculatedValue = (double)this.mathExpressionEngine.executeScript(expression + ";");
+		return calculatedValue;
+	}
+
+
+	/**
+	 * Calculate the specified math expression of "f(t)" form.
+	 * 
+	 * @param expression The math expression to be calculated.
+	 * @param t The parameter "t" of "f(t)".
+	 * @return The calculated value.
+	 */
+	public synchronized double calculateMathExpression(String expression, double t) throws VnanoException {
+		mathExpressionParameterPlugin.x = Double.NaN;
+		mathExpressionParameterPlugin.y = Double.NaN;
+		mathExpressionParameterPlugin.t = t;
+		double calculatedValue = (double)this.mathExpressionEngine.executeScript(expression + ";");
+		return calculatedValue;
+	}
+
+
+	/**
+	 * Create a new script engine of Vnano, and initialize it for calculating math expressions.
+	 * 
+	 * @param locale The locale of the user's environment, which determines the language of error messages.
+	 * @return An initialized engine.
+	 * @throws VnanoException Thrown when it failed to load/initialize plug-ins, etc.
+	 */
+	private final void initializeMathExpressionEngine(Locale locale) throws VnanoException {
+
+		// See also the tutorial guide for using Vnano, if necessary:
+		//     https://www.vcssl.org/en-us/vnano/doc/tutorial/
+
+		// Create a script engine of Vnano.
+		this.mathExpressionEngine = new VnanoEngine();
+
+		// Enable options for calculating math expressions.
+		// For details of each option, see:
+		//     https://www.vcssl.org/en-us/vnano/spec/#options
+		Map<String, Object> optionMap = new HashMap<String, Object>();
+		optionMap.put("LOCALE", locale);
+		optionMap.put("EVAL_INT_LITERAL_AS_FLOAT", true);
+		optionMap.put("EVAL_ONLY_EXPRESSION", true);
+		optionMap.put("EVAL_ONLY_FLOAT", true);
+		optionMap.put("MAIN_SCRIPT_NAME", "Input_Expression");
+		this.mathExpressionEngine.setOptionMap(optionMap);
+
+		// Configure permission items.
+		// Set "DENY" to the default, and set "ALLOW" to only items unnecessary for calculating math expression.
+		// For details of each option, see:
+		//     https://www.vcssl.org/en-us/vnano/spec/#permissions
+		Map<String, String> permissionMap = new HashMap<String, String>();
+		permissionMap.put(ConnectorPermissionName.DEFAULT, ConnectorPermissionValue.DENY);
+		/* If there is any permission item necessary for this engine's usage, "ALLOW" it here. */
+		this.mathExpressionEngine.setPermissionMap(permissionMap);
+
+		// Load plug-ins listed in "./plugin/VnanoPluginList.txt".
+		try {
+			PluginLoader pluginLoader = new PluginLoader("UTF-8");
+			pluginLoader.setPluginListPath("./plugin/VnanoPluginList.txt");
+			pluginLoader.load();
+			for (Object plugin: pluginLoader.getPluginInstances()) {
+				this.mathExpressionEngine.connectPlugin("___VNANO_AUTO_KEY", plugin);
+
+			}
+
+		// The locale setting does not affect to error messages occurred in PluginLoader,
+		// so set the locale to the exception explicitly.
+		} catch (VnanoException vne) {
+			vne.setLocale(locale);
+			throw vne;
+		}
+
+		// Instantiate a plug-in providing parameter variables (x, y, and t) available in math expressions,
+		// and connect it to the engine.
+		this.mathExpressionParameterPlugin = new MathExpressionParameterPlugin();
+		this.mathExpressionEngine.connectPlugin("MathExprParamPlugin", this.mathExpressionParameterPlugin);
+	}
+}
