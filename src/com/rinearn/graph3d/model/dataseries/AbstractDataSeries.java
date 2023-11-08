2a4d69865efe696161924138c71d8a7832d3375b
Topdev0706 <jacksontomas731@gmail.com>
2023-11-08 17:49:36 +0900

    Add: AbstractDataSeries: declare getX/Y/ZCoordinates()

--- src/com/rinearn/graph3d/model/dataseries/AbstractDataSeries.java
+++ src/com/rinearn/graph3d/model/dataseries/AbstractDataSeries.java
@@ -17,4 +17,32 @@ package com.rinearn.graph3d.model.dataseries;
 
 public abstract class AbstractDataSeries {
 
+	// !!!!! NOTE & TODO !!!!!
+	// ・double型ではなくBigDecimal型で座標値を持っている系列はどう扱うべき？
+	//   -> とりあえず double ベースである程度実装してみて、
+	//      上から下まで処理がつながって見通しが付いたあたりで、拡張するか書き直すか考える。
+	//      最初から分派させると工程が難解になり過ぎるし、たぶん結果も無駄に冗長気味になりそうなので。
+
+	/**
+	 * Gets the X-coordinate values of the points of this data series.
+	 * 
+	 * @return The X-coordinate values.
+	 */
+	public abstract double[][] getXCoordinates();
+
+
+	/**
+	 * Gets the Y-coordinate values of the points of this data series.
+	 * 
+	 * @return The Y-coordinate values.
+	 */
+	public abstract double[][] getYCoordinates();
+
+
+	/**
+	 * Gets the Z-coordinate values of the points of this data series.
+	 * 
+	 * @return The Z-coordinate values.
+	 */
+	public abstract double[][] getZCoordinates();
 }
