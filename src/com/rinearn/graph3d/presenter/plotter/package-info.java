ca31bc9e3efce60dfcf93c2c728d1278c8957044
Topdev0706 <jacksontomas731@gmail.com>
2023-11-17 13:22:10 +0900

    Add: new package: com.rinearn.graph3d.presenter.plotter
    The subpackage in Presenter layer, in which "plotter"s are bunched together.
    A plotter is an implementation of RinearnGraph3DPlottingListener interface.
    It is invoked when plottings/re-plottings are required on this application,
    and performs a part of plottings (e.g. plots points, or lines, and so on).

---
+++ src/com/rinearn/graph3d/presenter/plotter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The subpackage in Presenter layer, in which "plotter"s are bunched together.
+ * 
+ * A plotter is an object implementing RinearnGraph3DPlottingListener interface,
+ * performs a part of plottings/re-plottings (e.g. plots points, or lines, etc),
+ * in event-driven flow.
+ */
+package com.rinearn.graph3d.presenter.plotter;
