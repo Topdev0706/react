5b61fd01ae83647e32227a30c46cd0123a1d879f
Topdev0706 <jacksontomas731@gmail.com>
2023-12-14 23:24:42 +0900

    Add: AbstractDataSeries: temporary comment

--- src/com/rinearn/graph3d/model/dataseries/AbstractDataSeries.java
+++ src/com/rinearn/graph3d/model/dataseries/AbstractDataSeries.java
@@ -22,6 +22,8 @@ public abstract class AbstractDataSeries {
 	//   -> とりあえず double ベースである程度実装してみて、
 	//      上から下まで処理がつながって見通しが付いたあたりで、拡張するか書き直すか考える。
 	//      最初から分派させると工程が難解になり過ぎるし、たぶん結果も無駄に冗長気味になりそうなので。
+	//
+	// ・宣言するのすっかり忘れてたけど各点ごとの可視性の配列要るでしょ。
 
 	/**
 	 * Gets the X-coordinate values of the points of this data series.
